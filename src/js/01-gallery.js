c93eb94b64fc3d3aabe27b5c8235da0a86823a0c
Anna Kucherenko <kucherenko.place@icloud.com>
2023-03-15 09:05:25 +0200

    task1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,31 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
 
-console.log(galleryItems);
+const init = function () {
+  let galleryLi = '';
+  galleryItems.forEach(
+    item =>
+      (galleryLi += `<a class="gallery__item" href="${item.original}">
+        <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+        </a>
+        `)
+  );
+  return galleryLi;
+};
+
+const openModal = function (e) {
+  e.preventDefault();
+  const imageItem = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+  });
+};
+
+const galleryUl = document.querySelector('.gallery');
+
+galleryUl.innerHTML = init();
+
+galleryUl.addEventListener('click', openModal);
